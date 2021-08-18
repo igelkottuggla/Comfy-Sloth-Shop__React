ca756037123c2e6cb830de45e04964b5b4bc76f1
NataliaSova <rot.fuchslein@gmail.com>
2021-08-18 15:12:01 +0500

    Setting up the Router + Navbar

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from './HomePage';
+import About from './AboutPage';
+import Auth from './AuthWrapper';
+import Cart from './CartPage';
+import Checkout from './CheckoutPage';
+import Error from './ErrorPage';
+import PrivateRoute from './PrivateRoute';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+
+export {
+    Home,
+    About,
+    Auth,
+    Cart,
+    Checkout,
+    Error,
+    PrivateRoute,
+    Products,
+    SingleProduct,
+};
