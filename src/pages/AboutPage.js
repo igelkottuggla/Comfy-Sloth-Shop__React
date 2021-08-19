63494832ac0e3df3174b837d75a6fb05e14dc031
NataliaSova <rot.fuchslein@gmail.com>
2021-08-19 16:06:16 +0500

    Cart buttons, Footer, Sidebar, Products context setup, Toggle sidebar functionality, error page, about page, page hero

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,37 +1,58 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+    return (
+        <main>
+            <PageHero title='about' />
+            <Wrapper className='page section section-center'>
+                <img src={aboutImg} alt='nice desk' />
+                <article>
+                    <div className='title'>
+                        <h2>Our story</h2>
+                        <div className='underline'></div>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Corporis, obcaecati sed numquam rerum est repellendus
+                        unde reiciendis assumenda inventore perferendis, nisi
+                        nostrum dolorum eaque voluptatem atque dolores
+                        voluptatibus nobis quae. Voluptates libero totam
+                        inventore enim quae et quaerat dolores tempora?
+                    </p>
+                </article>
+            </Wrapper>
+        </main>
+    );
+};
 
 const Wrapper = styled.section`
-  display: grid;
-  gap: 4rem;
-  img {
-    width: 100%;
-    display: block;
-    border-radius: var(--radius);
-    height: 500px;
-    object-fit: cover;
-  }
-  p {
-    line-height: 2;
-    max-width: 45em;
-    margin: 0 auto;
-    margin-top: 2rem;
-    color: var(--clr-grey-5);
-  }
-  .title {
-    text-align: left;
-  }
-  .underline {
-    margin-left: 0;
-  }
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-  }
-`
-export default AboutPage
+    display: grid;
+    gap: 4rem;
+    img {
+        width: 100%;
+        display: block;
+        border-radius: var(--radius);
+        height: 500px;
+        object-fit: cover;
+    }
+    p {
+        line-height: 2;
+        max-width: 45em;
+        margin: 0 auto;
+        margin-top: 2rem;
+        color: var(--clr-grey-5);
+    }
+    .title {
+        text-align: left;
+    }
+    .underline {
+        margin-left: 0;
+    }
+    @media (min-width: 992px) {
+        grid-template-columns: 1fr 1fr;
+    }
+`;
+export default AboutPage;
