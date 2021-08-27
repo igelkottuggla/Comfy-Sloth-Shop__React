9f3ab0a4c4c8959cc7737dd151aeb776a0b6c6ef
NataliaSova <rot.fuchslein@gmail.com>
2021-08-27 16:34:32 +0500

    Filter context, List View, Sorting component & sorting functionality, filter component, filter reducer, filter functionality

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { useFilterContext } from '../context/filter_context';
+import { useProductsContext } from '../context/products_context';
+import Loading from './Loading';
+
 import GridView from './GridView';
 import ListView from './ListView';
 
 const ProductList = () => {
-    const { filtered_products: products } = useFilterContext();
+    const { filtered_products: products, grid_view } = useFilterContext();
+    const { products_loading: loading } = useProductsContext();
+
+    if (loading) return <Loading />;
+    else if (products.length < 1) {
+        return (
+            <h5 style={{ textTransform: 'none' }}>
+                Sorry, no products matched your search
+            </h5>
+        );
+    } else if (grid_view === false) {
+        return <ListView products={products} />;
+    }
     return <GridView products={products}></GridView>;
 };
 
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,94 @@ import {
 
 const filter_reducer = (state, action) => {
     if (action.type === LOAD_PRODUCTS) {
+        let maxPrice = action.payload.map((product) => product.price);
+        maxPrice = Math.max(...maxPrice);
         return {
             ...state,
             all_products: [...action.payload],
             filtered_products: [...action.payload],
+            filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
+        };
+    } else if (action.type === SET_GRIDVIEW) {
+        return { ...state, grid_view: true };
+    } else if (action.type === SET_LISTVIEW) {
+        return { ...state, grid_view: false };
+    } else if (action.type === UPDATE_SORT) {
+        return { ...state, sort: action.payload };
+    } else if (action.type === SORT_PRODUCTS) {
+        const { sort, filtered_products } = state;
+        let tempProducts = [...filtered_products];
+        if (sort === 'price-lowest') {
+            tempProducts = tempProducts.sort((a, b) => a.price - b.price);
+        } else if (sort === 'price-highest') {
+            tempProducts = tempProducts.sort((a, b) => b.price - a.price);
+        } else if (sort === 'name-a') {
+            tempProducts = tempProducts.sort((a, b) => {
+                return a.name.localeCompare(b.name);
+            });
+        } else {
+            tempProducts = tempProducts.sort((a, b) => {
+                return b.name.localeCompare(a.name);
+            });
+        }
+        return { ...state, filtered_products: tempProducts };
+    } else if (action.type === UPDATE_FILTERS) {
+        const { name, value } = action.payload;
+        return { ...state, filters: { ...state.filters, [name]: value } };
+    } else if (action.type === FILTER_PRODUCTS) {
+        const { all_products } = state;
+        const { text, category, color, company, price, shipping } =
+            state.filters;
+        let tempProducts = [...all_products];
+
+        if (text) {
+            tempProducts = tempProducts.filter((product) => {
+                return product.name.toLowerCase().startsWith(text);
+            });
+        }
+        if (price) {
+            tempProducts = tempProducts.filter((product) => {
+                return product.price <= price;
+            });
+        }
+
+        if (category !== 'all') {
+            tempProducts = tempProducts.filter((product) => {
+                return product.category === category;
+            });
+        }
+
+        if (company !== 'all') {
+            tempProducts = tempProducts.filter((product) => {
+                return product.company === company;
+            });
+        }
+
+        if (color !== 'all') {
+            tempProducts = tempProducts.filter((product) => {
+                return product.colors.find((col) => col === color);
+            });
+        }
+
+        if (shipping) {
+            tempProducts = tempProducts.filter((product) => {
+                return product.shipping === true;
+            });
+        }
+
+        return { ...state, filtered_products: tempProducts };
+    } else if (action.type === CLEAR_FILTERS) {
+        return {
+            ...state,
+            filters: {
+                ...state.filters,
+                text: '',
+                company: 'all',
+                category: 'all',
+                color: 'all',
+                price: state.filters.max_price,
+                shipping: false,
+            },
         };
     }
 
