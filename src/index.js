be455001d1456e9bf37516d8557efd711ad8b6a6
NataliaSova <rot.fuchslein@gmail.com>
2022-01-31 12:43:00 +0500

    Fixing Auth issues

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
     <Auth0Provider
-        domain={process.env.REACT_APP_AUTH_DOMAIN}
+        domain={process.env.REACT_APP_DOMAIN}
         clientId={process.env.REACT_APP_CLIENT_ID}
         redirectUri={window.location.origin}
         cacheLocation='localstorage'
